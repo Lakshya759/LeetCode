2c4fd44acaeaba1c9a0d3b95193231ba89df0436
Lakshya759 <lakshyasri759@gmail.com>
2026-09-13 20:28:06 +0530

    Time: 4 ms (44.92%) | Memory: 22.1 MB (64.72%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -5,13 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    res=0
     def solve(self,root):
         if(root is None):
             return [0,0]
         [maxl,l]=self.solve(root.left)
         [maxr,r]=self.solve(root.right)
         return [max(maxl,max(maxr,l+r)),max(l,r)+1]
+    def solve1(self,root):
+        if(root is None):
+            return 0
+        l=self.solve1(root.left)
+        r=self.solve1(root.right)
+        self.res=max(self.res,l+r)
+        return max(l,r)+1
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        [maxi,height]=self.solve(root)
-        return maxi
+        # [maxi,height]=self.solve(root)
+        # return maxi
+        self.solve1(root)
+        return self.res
         
